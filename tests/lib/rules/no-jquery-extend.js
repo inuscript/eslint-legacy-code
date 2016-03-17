38e9b36935bf573e2f35950347847dfe3d90e422
inuscript <inuscript@gmail.com>
2016-03-17 23:42:01 +0900

    lint

--- tests/lib/rules/no-jquery-extend.js
+++ tests/lib/rules/no-jquery-extend.js
@@ -4,38 +4,38 @@
  * @copyright 2016 inuscript. All rights reserved.
  * See LICENSE file in root directory for full license.
  */
-"use strict";
+'use strict'
 
-//------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------
 // Requirements
-//------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------
 
-var rule = require("../../../lib/rules/no-jquery-extend"),
+var rule = require('../../../lib/rules/no-jquery-extend'),
 
-    RuleTester = require("eslint").RuleTester;
+  RuleTester = require('eslint').RuleTester
 
 
-//------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------
 // Tests
-//------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------
 
-var ruleTester = new RuleTester();
-ruleTester.run("no-jquery-extend", rule, {
+var ruleTester = new RuleTester()
+ruleTester.run('no-jquery-extend', rule, {
 
-    valid: [
-        {
-            code: "Object.assign(objA, objB)",
-        }
+  valid: [
+    {
+      code: 'Object.assign(objA, objB)',
+    }
         // give me some code that won't trigger a warning
-    ],
+  ],
 
-    invalid: [
-        {
-            code: "$.extend(objA, objB)",
-            errors: [{
-                message: "Use Object.assign instead of jQuery.extend",
-                type: "CallExpression"
-            }]
-        }
-    ]
-});
+  invalid: [
+    {
+      code: '$.extend(objA, objB)',
+      errors: [{
+        message: 'Use Object.assign instead of jQuery.extend',
+        type: 'CallExpression'
+      }]
+    }
+  ]
+})
