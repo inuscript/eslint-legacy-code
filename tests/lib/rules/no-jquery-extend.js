8655d534ffdd96a5acc974159766df7bc52b9641
inuscript <inuscript@gmail.com>
2016-03-17 22:55:53 +0900

    add

---
+++ tests/lib/rules/no-jquery-extend.js
@@ -0,0 +1,39 @@
+/**
+ * @fileoverview find jQuery.extend and notice for use Objec.assign
+ * @author inuscript
+ * @copyright 2016 inuscript. All rights reserved.
+ * See LICENSE file in root directory for full license.
+ */
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+var rule = require("../../../lib/rules/no-jquery-extend"),
+
+    RuleTester = require("eslint").RuleTester;
+
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+var ruleTester = new RuleTester();
+ruleTester.run("no-jquery-extend", rule, {
+
+    valid: [
+
+        // give me some code that won't trigger a warning
+    ],
+
+    invalid: [
+        {
+            code: "$.extend(objA, objB)",
+            errors: [{
+                message: "Fill me in.",
+                type: "Me too"
+            }]
+        }
+    ]
+});
